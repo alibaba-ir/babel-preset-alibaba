a46f87fb4becaf0f38abeb19fc578ee17542fc50
Ali Torki <A.Torki@alibaba.ir>
2017-06-22 15:35:47 +0430

    fix: Falsy value in array

--- index.js
+++ index.js
@@ -3,20 +3,22 @@
 module.exports = function buildAlibabaPreset(context, options) {
 	return {
 		presets: [
-			require('babel-preset-env'),
-			{
-				targets: {
-					// React parses on ie 9, so we should too
-					ie: 9,
-					// We currently minify with uglify
-					// Remove after https://github.com/mishoo/UglifyJS2/issues/448
-					uglify: true,
-				},
-				// Disable polyfill transforms
-				useBuiltIns: false,
-				// Do not transform modules to CJS
-				modules: false,
-			},
+			[
+				require('babel-preset-env'),
+				{
+					targets: {
+						// React parses on ie 9, so we should too
+						ie: 9,
+						// We currently minify with uglify
+						// Remove after https://github.com/mishoo/UglifyJS2/issues/448
+						uglify: true,
+					},
+					// Disable polyfill transforms
+					useBuiltIns: false,
+					// Do not transform modules to CJS
+					modules: false,
+				}
+			],
 			require('babel-preset-es2017'),
 			require('babel-preset-react'),
 		],
@@ -31,7 +33,7 @@ module.exports = function buildAlibabaPreset(context, options) {
 			// https://github.com/babel/babel/tree/master/packages/babel-plugin-syntax-dynamic-import
 			require('babel-plugin-syntax-dynamic-import'),
 			require('babel-plugin-transform-class-properties'),
-			[require('babel-plugin-transform-runtime'), {helpers: false, polyfill: false, regenerator: true }]
+			[require('babel-plugin-transform-runtime'), {helpers: false, polyfill: false, regenerator: true }],
 
 			[require('babel-plugin-transform-es2015-template-literals'), { spec: true }],
 			require('babel-plugin-transform-es3-member-expression-literals'),
